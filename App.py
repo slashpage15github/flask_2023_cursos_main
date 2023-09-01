d7a049e7b99698ecee57459d8fcb4b77253c9fcf
slashpage15github <107419267+slashpage15github@users.noreply.github.com>
2023-09-01 15:28:18 -0600

    inicio de proceso insert aspirante

--- App.py
+++ App.py
@@ -11,5 +11,10 @@ def index():
 def add_aspirante():
     return render_template('aspirante.html')
 
+@app.route("/registra",methods=['POST'])
+def registra_aspirante():
+    _rfc=request.form['f_rfc']
+    return _rfc
+
 if __name__ == "__main__":
     app.run(debug=True)
