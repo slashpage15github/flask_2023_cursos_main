de6e02c4e5343f286f040fa8b4f4fa8bc6dfb5e0
slashpage15github <107419267+slashpage15github@users.noreply.github.com>
2023-09-14 17:02:47 -0600

    backend trying

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
-
+import model.package_model.aspirantes as aspirantes
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,8 +13,21 @@ def add_aspirante():
 
 @app.route("/registra",methods=['POST'])
 def registra_aspirante():
-    _rfc=request.form['f_rfc']
-    return _rfc
+    _rfc=request.form['f_rfc'].upper()
+    _nom=request.form['f_nombre'].upper()    
+    _pat=request.form['f_paterno'].upper()    
+    _mat=request.form['f_materno'].upper()    
+    _emp=request.form['f_id_empresa']    
+    _tel=request.form['f_telefono']    
+    _ema=request.form['f_email']    
+    _cur=request.form['f_id_curso']
+    #obj_asp= aspirantes.Aspirantes(_rfc,_nom,_pat,_mat,_emp,_tel,_ema,'1982-10-10')    
+    obj_asp= aspirantes.Aspirantes(_rfc,_nom,_pat,_mat,_emp,_tel,_ema,'1982-10-10')
+    res=obj_asp.insertar_student(obj_asp)
+    #result=obj_asp.insertar_student(obj_asp)
+    #print(result)
+    #return _rfc+'-'+_nom+'-'+_pat+'-'+_mat+'-'+_emp+'-'+_tel+'-'+_ema+'-'+_cur
+    return res
 
 if __name__ == "__main__":
     app.run(debug=True)
--- model/package_model/aspirantes.py
+++ model/package_model/aspirantes.py
@@ -1,16 +1,28 @@
 import model.package_model.Database as Database
-class Student :
-    def insertar_student(self, nom, bra, rol,sec,age):
-        conexion = Database.Database()
-        with conexion.cursor as cursor:
-            affected=cursor.execute("INSERT INTO student(name, branch, roll, section, age) VALUES (%s, %s, %s, %s, %s)",
-                        (nom, bra, rol,sec,age))
-        conexion.conn.commit()
-        conexion.conn.close()
-        return affected
+class Aspirantes:
+    def __init__(self, rfc, nombre, paterno, materno,id_empresa, telefono, email, fecha_registro):
+        self.__rfc=rfc
+        self.__nombre=nombre
+        self.__paterno=paterno
+        self.__materno=materno
+        self.__id_empresa=id_empresa
+        self.__telefono=telefono
+        self.__email=email
+        self.__fecha_registro=fecha_registro
+    
+    def insertar_student(self, obj_asp):
+        return obj_asp.__rfc
+        #return obj_asp
+        # conexion = Database.Database()
+        # with conexion.cursor as cursor:
+        #     affected=cursor.execute("INSERT INTO student(name, branch, roll, section, age) VALUES (%s, %s, %s, %s, %s)",
+        #                 (nom, bra, rol,sec,age))
+        # conexion.conn.commit()
+        # conexion.conn.close()
+        # return affected
     
     def eliminar_student(self,id):
-        conexion = Databasex.Databasex()
+        conexion = Database.Database()
         with conexion.cursor as cursor:
             affected=cursor.execute("DELETE FROM student WHERE NAME = %s", (id))
         conexion.conn.commit()
@@ -18,7 +30,7 @@ class Student :
         return affected
     
     def actualizar_student(self, bra, rol,sec,age,nom ):
-        conexion = Databasex.Databasex()
+        conexion = Database.Database()
         with conexion.cursor as cursor:
             affected=cursor.execute("UPDATE student SET BRANCH = %s, ROLL = %s, SECTION = %s, AGE = %s WHERE NAME = %s",
                        (bra, rol, sec, age, nom))
@@ -27,7 +39,7 @@ class Student :
         return affected
 
     def obtener_students(self):
-        conexion = Databasex.Databasex()
+        conexion = Database.Database()
         students = []
         with conexion.cursor as cursor:
             cursor.execute("SELECT name, branch, roll, section, age FROM student")
@@ -36,7 +48,7 @@ class Student :
         return students
     
     def obtener_student_por_id(self,id):
-        conexion = conexion = Databasex.Databasex()
+        conexion = conexion = Database.Database()
         student = None
         with conexion.cursor as cursor:
             cursor.execute(
